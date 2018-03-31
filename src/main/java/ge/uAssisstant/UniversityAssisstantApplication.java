075ea30c997619866186f1e0e2e3d7598e01f2d1
Levan <lgode14@freeuni.edu.ge>
2018-03-31 12:55:27 +0400

    initial commit

---
+++ src/main/java/ge/uAssisstant/UniversityAssisstantApplication.java
@@ -0,0 +1,12 @@
+package ge.uAssisstant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversityAssisstantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UniversityAssisstantApplication.class, args);
+    }
+}
