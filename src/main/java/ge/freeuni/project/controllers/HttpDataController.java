dd9878c96447ed7a819403963b15c72e625136fb
Levan <lgode14@freeuni.edu.ge>
2018-05-01 02:12:12 +0400

    http request

--- src/main/java/ge/freeuni/project/controllers/HttpDataController.java
+++ src/main/java/ge/freeuni/project/controllers/HttpDataController.java
@@ -1,25 +1,27 @@
 package ge.freeuni.project.controllers;
 
-import org.apache.commons.httpclient.*;
-import org.apache.commons.httpclient.methods.GetMethod;
-import org.apache.commons.httpclient.params.HttpMethodParams;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+import static org.springframework.http.MediaType.TEXT_HTML_VALUE;
+
 
 @Controller
-public class HttpDataController {
+public class HttpDataController  {
 
-    @GetMapping("/mes-gov-ge")
+    @RequestMapping(value = "/mes-gov-ge", method = RequestMethod.GET, produces = "text/plain")
     public String getContent(HttpServletResponse response){
 //        Runtime rt = Runtime.getRuntime();
 //        try {
-//            Process pr = rt.exec("wget -p -k 'http://mes.gov.ge/content.php?id=75&lang=geo' --directory-prefix 'src/main/resources/static'");
+//            Process pr = rt.exec("wget -p -k \"http://mes.gov.ge/content.php?id=75&lang=geo\" --directory-prefix \"src/main/resources/static\"");
 //            pr.waitFor();
 //            System.out.println("Page downloaded");
 //        } catch (IOException e) {
@@ -28,10 +30,6 @@ public class HttpDataController {
 //            e.printStackTrace();
 //        }
 
-        response.setHeader("Content-Type","text/html");
-        return "index";
-
-
-
+        return "mes.gov.ge/content.php@id=75&lang=geo";
     }
 }
