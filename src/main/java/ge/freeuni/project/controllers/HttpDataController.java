56f34bd4ae4ee209f5b432593a863db686d3004f
levangode <lgode14@freeuni.edu.ge>
2018-05-01 14:45:31 +0400

    working on resources

--- src/main/java/ge/freeuni/project/controllers/HttpDataController.java
+++ src/main/java/ge/freeuni/project/controllers/HttpDataController.java
@@ -1,35 +1,66 @@
 package ge.freeuni.project.controllers;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.MediaType;
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
-import static org.springframework.http.MediaType.TEXT_HTML_VALUE;
+import static org.springframework.http.MediaType.TEXT_HTML;
 
 
 @Controller
-public class HttpDataController  {
+public class HttpDataController extends WebMvcAutoConfiguration {
 
-    @RequestMapping(value = "/mes-gov-ge", method = RequestMethod.GET, produces = "text/plain")
-    public String getContent(HttpServletResponse response){
-//        Runtime rt = Runtime.getRuntime();
-//        try {
-//            Process pr = rt.exec("wget -p -k \"http://mes.gov.ge/content.php?id=75&lang=geo\" --directory-prefix \"src/main/resources/static\"");
-//            pr.waitFor();
-//            System.out.println("Page downloaded");
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
-
-        return "mes.gov.ge/content.php@id=75&lang=geo";
+    @RequestMapping(value = "/mes-gov-ge", method = RequestMethod.GET)
+    public ResponseEntity<String> getContent(HttpServletResponse response){
+        try {
+            List<String> command = new ArrayList<>();
+            command.add("wget");
+            command.add("-p");
+            command.add("-k");
+            command.add("-nH");
+            command.add("\"http://mes.gov.ge/content.php?id=75&lang=geo\"");
+            command.add("--directory-prefix");
+            command.add("\"src/main/resources/public\"");
+            ProcessBuilder builder = new ProcessBuilder(command);
+            builder.directory(new File("."));
+            final Process process = builder.start();
+            try {
+                process.waitFor();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            InputStream is = process.getInputStream();
+            InputStreamReader isr = new InputStreamReader(is);
+            BufferedReader br = new BufferedReader(isr);
+            String line;
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+            System.out.println("Program terminated!");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        HttpHeaders responseHeaders = new HttpHeaders();
+        responseHeaders.setContentType(TEXT_HTML);
+        String body = getFile("public/content.php?id=75&lang=geo");
+        return new ResponseEntity<String>( body, responseHeaders, HttpStatus.OK);
     }
+
+    private String getFile(String fileName) {
+
+        String text = new Scanner(getClass().getClassLoader().getResourceAsStream(fileName), "UTF-8").useDelimiter("\\A").next();
+        return text;
+    }
+
+
+
 }
