3d3842ffb52f9445532bb1fb7bcc1bb1a6334c93
Levan <lgode14@freeuni.edu.ge>
2018-05-09 02:18:21 +0400

    added interests

--- front-angular/src/app/user-page/interests/interests.component.ts
+++ front-angular/src/app/user-page/interests/interests.component.ts
@@ -7,10 +7,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InterestsComponent implements OnInit {
 
+  interests: Array<string> = [
+    "ისტორია",
+    "ქართული",
+    "მათემატიკა",
+    "ქიმია",
+    "ბიოლოგია",
+    "მედიცინა",
+    "სპორტი",
+    "გეოგრაფია",
+  ];
+
+  myInterests: Array<string> = [
+
+  ];
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  getInterests(){
+    return this.interests;
+  }
+
+  getMyInterests(){
+    return this.myInterests;
+  }
+
+  addInterest(interest: string) {
+    this.myInterests.push(interest);
+    var index = this.interests.indexOf(interest, 0);
+    if (index > -1) {
+      this.interests.splice(index, 1);
+    }
+  }
+
+  removeInterest(interest: string) {
+    this.interests.push(interest);
+    var index = this.myInterests.indexOf(interest, 0);
+    if (index > -1) {
+      this.myInterests.splice(index, 1);
+    }
+  }
 }
