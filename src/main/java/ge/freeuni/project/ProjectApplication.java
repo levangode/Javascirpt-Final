1e5ffbc820e4798e0c41f5a11246d944ebcc1ed0
levangode <lgode14@freeuni.edu.ge>
2018-05-24 19:18:38 +0400

    //

--- src/main/java/ge/freeuni/project/ProjectApplication.java
+++ src/main/java/ge/freeuni/project/ProjectApplication.java
@@ -2,6 +2,7 @@ package ge.freeuni.project;
 
 import ge.freeuni.project.domain.InterestRepository;
 import ge.freeuni.project.domain.UserRepository;
+import ge.freeuni.project.models.UserInterest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -26,9 +27,15 @@ public class ProjectApplication implements CommandLineRunner {
     @Override
     @Transactional()
     public void run(String... args) throws Exception {
-//        interestRepository.save(new UserInterest("ბიოლოგია"));
-//        interestRepository.save(new UserInterest("ქიმია"));
-//        interestRepository.save(new UserInterest("ფიზიკა"));
+        interestRepository.save(new UserInterest("ბიოლოგია"));
+        interestRepository.save(new UserInterest("ქიმია"));
+        interestRepository.save(new UserInterest("ფიზიკა"));
+        interestRepository.save(new UserInterest("სოციოლოგია"));
+        interestRepository.save(new UserInterest("ფსიქოლოგია"));
+        interestRepository.save(new UserInterest("ისტორია"));
+        interestRepository.save(new UserInterest("კულტურა"));
+        interestRepository.save(new UserInterest("ბიზნესი"));
+        interestRepository.save(new UserInterest("მენეჯმენტი"));
 
     }
 }
