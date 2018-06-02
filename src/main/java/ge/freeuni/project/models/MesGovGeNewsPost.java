aa0fb1dd78b493ab9c254212df19b832c318298e
giorgigona <ggona14@freeuni.edu.ge>
2018-06-02 17:06:07 +0400

    add constructor, getter and setters in MesGovGeNewsPost class

--- src/main/java/ge/freeuni/project/models/MesGovGeNewsPost.java
+++ src/main/java/ge/freeuni/project/models/MesGovGeNewsPost.java
@@ -5,7 +5,6 @@ import lombok.*;
 
 
 @NoArgsConstructor
-@AllArgsConstructor
 @ToString
 @Getter
 @Setter
@@ -17,4 +16,56 @@ public class MesGovGeNewsPost {
     private String newsText;
     private String newsImgSrc;
 
+
+    public MesGovGeNewsPost(String newsTitleText, String newsTitleRef, String newsDate, String newsText, String newsImgSrc) {
+        this.newsTitle = newsTitleText;
+        this.newsTitleRef = newsTitleRef;
+        this.newsDate = newsDate;
+        this.newsText = newsText;
+        this.newsImgSrc = newsImgSrc;
+    }
+
+    public String getNewsTitle() {
+        return newsTitle;
+    }
+
+    public String getNewsDate() {
+        return newsDate;
+    }
+
+    public String getNewsTitleRef() {
+        return newsTitleRef;
+    }
+
+    public void setNewsTitleRef(String newsTitleRef) {
+        this.newsTitleRef = newsTitleRef;
+    }
+
+    public void setNewsDate(String newsDate) {
+
+        this.newsDate = newsDate;
+    }
+
+    public void setNewsTitle(String newsTitle) {
+
+        this.newsTitle = newsTitle;
+    }
+
+    public void setNewsImgSrc(String newsImgSrc) {
+        this.newsImgSrc = newsImgSrc;
+    }
+
+    public void setNewsText(String newsText) {
+        this.newsText = newsText;
+    }
+
+    public String getNewsImgSrc() {
+
+        return newsImgSrc;
+    }
+
+    public String getNewsText() {
+
+        return newsText;
+    }
 }
--- src/main/java/ge/freeuni/project/models/UserData.java
+++ src/main/java/ge/freeuni/project/models/UserData.java
@@ -14,5 +14,55 @@ public class UserData {
     private String image;
     private String provider;
 
+    public String getId() {
+        return id;
+    }
 
+    public String getProvider() {
+        return provider;
+    }
+
+    public void setProvider(String provider) {
+        this.provider = provider;
+    }
+
+    public String getImage() {
+
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public String getEmail() {
+
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
