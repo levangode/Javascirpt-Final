d7fe23351871eb57f7d11bf3bfb65a7e0af1b95d
levangode <lgode14@freeuni.edu.ge>
2018-06-11 21:55:07 +0400

    events control panel started, needs refactor

--- front-angular/src/app/university-control-panel/events-control/events-control.component.ts
+++ front-angular/src/app/university-control-panel/events-control/events-control.component.ts
@@ -1,25 +1,74 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {UniversityService} from "../../_services/university.service";
+import {animate, keyframes, query, stagger, style, transition, trigger} from "@angular/animations";
 
 @Component({
   selector: 'app-events-control',
   templateUrl: './events-control.component.html',
-  styleUrls: ['./events-control.component.scss']
+  styleUrls: ['./events-control.component.scss'],
+  animations: [
+    trigger('news', [
+      transition('* => *', [
+        query(':enter', style({opacity: 0}), {optional: true}),
+        query(':enter', stagger('300ms', [
+          animate('.6s ease-in', keyframes([
+            style({opacity: 0, transform: 'translateY(-75%)', offset: 0}),
+            style({opacity: .5, transform: 'translateY(35px)', offset: .3}),
+            style({opacity: 1, transform: 'translateY(0)', offset: 1}),
+          ]))
+        ]), {optional: true}),
+        query(':leave', stagger('300ms', [
+          animate('.6s ease-in', keyframes([
+            style({opacity: 0, transform: 'translateY(0)', offset: 0}),
+            style({opacity: .5, transform: 'translateY(35px)', offset: .3}),
+            style({opacity: 1, transform: 'translateY(-75%)', offset: 1}),
+          ]))
+        ]), {optional: true})
+      ])
+    ]),
+    trigger('creation', [
+      transition('* => *', [
+        query(':enter', style({opacity: 0}), {optional: true}),
+        query(':enter', stagger('300ms', [
+          animate('.6s ease-in', keyframes([
+            style({opacity: 0, transform: 'translateY(-75%)', offset: 0}),
+            style({opacity: .5, transform: 'translateY(-55%)', offset: .3}),
+            style({opacity: 1, transform: 'translateY(0)', offset: 1}),
+          ]))
+        ]), {optional: true})
+      ])
+    ])
+  ]
 })
 export class EventsControlComponent implements OnInit {
 
   events: Array<any>;
-  constructor(private universityService: UniversityService) { }
+  model: any = {};
+  loading = false;
+  showCreatePanel = false;
+
+  constructor(private universityService: UniversityService) {
+    this.events = [];
+  }
 
   ngOnInit() {
     this.universityService.getCurrentUniversitySubject().subscribe(value => {
       this.events = this.universityService.getCurrentUniversity().universityEvents;
       console.log(this.events);
     });
-    if(this.universityService.getCurrentUniversitySubject().isStopped){
+    if (this.universityService.getCurrentUniversitySubject().isStopped) {
       this.events = this.universityService.getCurrentUniversity().universityEvents;
       console.log(this.events);
     }
+  }
+
+  changeState() {
+    this.showCreatePanel = !this.showCreatePanel;
+    this.model = {};
+  }
+
+  createEvent(){
+
   }
 
 }
