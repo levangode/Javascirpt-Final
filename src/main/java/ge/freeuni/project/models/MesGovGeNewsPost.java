df929b451e5f727f6bb2678e225c78dfc5fdfb1d
levangode <lgode14@freeuni.edu.ge>
2018-06-14 19:16:15 +0400

    fix model remove lombok plugin

--- src/main/java/ge/freeuni/project/models/MesGovGeNewsPost.java
+++ src/main/java/ge/freeuni/project/models/MesGovGeNewsPost.java
@@ -4,11 +4,6 @@ package ge.freeuni.project.models;
 import lombok.*;
 
 
-@NoArgsConstructor
-@AllArgsConstructor
-@ToString
-@Getter
-@Setter
 public class MesGovGeNewsPost {
 
     private String newsTitle;
@@ -17,4 +12,54 @@ public class MesGovGeNewsPost {
     private String newsText;
     private String newsImgSrc;
 
+    public MesGovGeNewsPost() {
+    }
+
+    public MesGovGeNewsPost(String newsTitle, String newsTitleRef, String newsDate, String newsText, String newsImgSrc) {
+        this.newsTitle = newsTitle;
+        this.newsTitleRef = newsTitleRef;
+        this.newsDate = newsDate;
+        this.newsText = newsText;
+        this.newsImgSrc = newsImgSrc;
+    }
+
+    public String getNewsTitle() {
+        return newsTitle;
+    }
+
+    public void setNewsTitle(String newsTitle) {
+        this.newsTitle = newsTitle;
+    }
+
+    public String getNewsTitleRef() {
+        return newsTitleRef;
+    }
+
+    public void setNewsTitleRef(String newsTitleRef) {
+        this.newsTitleRef = newsTitleRef;
+    }
+
+    public String getNewsDate() {
+        return newsDate;
+    }
+
+    public void setNewsDate(String newsDate) {
+        this.newsDate = newsDate;
+    }
+
+    public String getNewsText() {
+        return newsText;
+    }
+
+    public void setNewsText(String newsText) {
+        this.newsText = newsText;
+    }
+
+    public String getNewsImgSrc() {
+        return newsImgSrc;
+    }
+
+    public void setNewsImgSrc(String newsImgSrc) {
+        this.newsImgSrc = newsImgSrc;
+    }
 }
