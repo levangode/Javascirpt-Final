1684a5615b1d26d2ec6caac30214913e54300027
levangode <lgode14@freeuni.edu.ge>
2018-07-05 14:39:29 +0400

    refactored some js code, added navigation menu in university profile

--- web/public/js/app.js
+++ web/public/js/app.js
@@ -1,18 +1,18 @@
 const router = undefined;
 window.addEventListener('load', () => {
-    const el = $('#app');
+    const el = document.getElementById("app");
 
     // Compile Handlebar Templates
-    const errorTemplate = Handlebars.compile($('#error-template').html());
-    const universitiesTemplate = Handlebars.compile($('#universities-template').html());
-    const ratingsTemplate = Handlebars.compile($('#ratings-template').html());
-    const homeTemplate = Handlebars.compile($('#home-template').html());
-    const universityProfileTemplate = Handlebars.compile($('#university-profile-template').html());
-    const universityEventsTemplate = Handlebars.compile($('#university-events-template').html());
-    const universityNewsTemplate = Handlebars.compile($('#university-news-template').html());
-    const universityFacultiesTemplate = Handlebars.compile($('#university-faculties-template').html());
-    const universityFaqTemplate = Handlebars.compile($('#university-faq-template').html());
-    const universityAboutTemplate = Handlebars.compile($('#university-about-template').html());
+    const errorTemplate = Handlebars.compile(document.getElementById('error-template').innerHTML);
+    const universitiesTemplate = Handlebars.compile(document.getElementById('universities-template').innerHTML);
+    const ratingsTemplate = Handlebars.compile(document.getElementById('ratings-template').innerHTML);
+    const homeTemplate = Handlebars.compile(document.getElementById('home-template').innerHTML);
+    const universityProfileTemplate = Handlebars.compile(document.getElementById('university-profile-template').innerHTML);
+    const universityEventsTemplate = Handlebars.compile(document.getElementById('university-events-template').innerHTML);
+    const universityNewsTemplate = Handlebars.compile(document.getElementById('university-news-template').innerHTML);
+    const universityFacultiesTemplate = Handlebars.compile(document.getElementById('university-faculties-template').innerHTML);
+    const universityFaqTemplate = Handlebars.compile(document.getElementById('university-faq-template').innerHTML);
+    const universityAboutTemplate = Handlebars.compile(document.getElementById('university-about-template').innerHTML);
 
     // Router Declaration
     this.router = new Router({
@@ -23,7 +23,7 @@ window.addEventListener('load', () => {
                 title: 'Error 404 - Page NOT Found!',
                 message: `The path '/${path}' does not exist on this site`,
             });
-            el.html(html);
+            el.innerHTML = html;
         },
     });
 
@@ -38,129 +38,148 @@ window.addEventListener('load', () => {
     const showError = (error) => {
         const {title, message} = error.response.data;
         const html = errorTemplate({color: 'red', title, message});
-        el.html(html);
+        el.innerHTML = html;
     };
 
 
     this.router.add('/', async () => {
         // Display loader first
         let html = homeTemplate();
-        el.html(html);
+        el.innerHTML = html;
         try {
             const responseNews = await api.get('/news');
             const news = responseNews.data;
 
             console.log(news);
             html = homeTemplate({news});
-            el.html(html);
+            el.innerHTML = html;
         } catch (error) {
             showError(error);
         } finally {
-            // Remove loader status
-            $('.loading').removeClass('loading');
+            removeLoadingClass();
         }
     });
 
     this.router.add('/ratings', () => {
         let html = ratingsTemplate();
-        el.html(html);
+        el.innerHTML = html;
     });
 
     this.router.add('/university/(:any)/events', async (id) => {
         let html = universityEventsTemplate();
-        el.html(html);
+        el.innerHTML = html;
         try {
-            const responseUniversity = await api.get('/university/'+id);
+            const responseUniversity = await api.get('/university/' + id);
             const university = responseUniversity.data;
             console.log(university);
             html = universityEventsTemplate({university});
-            el.html(html);
-        }catch (error) {
+            el.innerHTML = html;
+        } catch (error) {
             showError(error)
         } finally {
-            $('.loading').removeClass('loading');
+            removeLoadingClass();
         }
     });
+
+    this.router.add('/university/(:any)', async (id) => {
+        let html = universityProfileTemplate();
+        el.innerHTML = html;
+        try {
+            const responseUniversity = await api.get('/university/' + id);
+            const university = responseUniversity.data;
+            console.log(university);
+            html = universityProfileTemplate({university});
+            el.innerHTML = html;
+        } catch (error) {
+            showError(error)
+        } finally {
+            removeLoadingClass();
+        }
+    });
+
     this.router.add('/university/(:any)/faculties', async (id) => {
         let html = universityFacultiesTemplate();
-        el.html(html);
+        el.innerHTML = html;
         try {
-            const responseUniversity = await api.get('/university/'+id);
+            const responseUniversity = await api.get('/university/' + id);
             const university = responseUniversity.data;
             console.log(university);
             html = universityFacultiesTemplate({university});
-            el.html(html);
-        }catch (error) {
+            el.innerHTML = html;
+        } catch (error) {
             showError(error)
         } finally {
-            $('.loading').removeClass('loading');
+            removeLoadingClass();
         }
     });
     this.router.add('/university/(:any)/faq', async (id) => {
         let html = universityFaqTemplate();
-        el.html(html);
+        el.innerHTML = html;
         try {
-            const responseUniversity = await api.get('/university/'+id);
+            const responseUniversity = await api.get('/university/' + id);
             const university = responseUniversity.data;
             console.log(university);
             html = universityFaqTemplate({university});
-            el.html(html);
-        }catch (error) {
+            el.innerHTML = html;
+        } catch (error) {
             showError(error)
         } finally {
-            $('.loading').removeClass('loading');
+            removeLoadingClass();
         }
     });
 
     this.router.add('/university/(:any)/about', async (id) => {
         let html = universityAboutTemplate();
-        el.html(html);
+        el.innerHTML = html;
         try {
-            const responseUniversity = await api.get('/university/'+id);
+
+            const responseUniversity = await api.get('/university/' + id);
             const university = responseUniversity.data;
             console.log(university);
+
             html = universityAboutTemplate({university});
-            el.html(html);
+            el.innerHTML = html;
             var about = document.getElementById("about-div");
             about.innerHTML = university.about;
-        }catch (error) {
+        } catch (error) {
             showError(error)
         } finally {
-            $('.loading').removeClass('loading');
+            removeLoadingClass();
         }
     });
 
     this.router.add('/university/(:any)/news', async (id) => {
         let html = universityNewsTemplate();
-        el.html(html);
+        el.innerHTML = html;
         try {
-            const responseUniversity = await api.get('/university/'+id);
+            const responseUniversity = await api.get('/university/' + id);
             const university = responseUniversity.data;
             console.log(university);
             html = universityNewsTemplate({university});
-            el.html(html);
-        }catch (error) {
+            el.innerHTML = html;
+        } catch (error) {
             showError(error)
         } finally {
-            $('.loading').removeClass('loading');
+            removeLoadingClass();
         }
     });
 
     this.router.add('/universities', async () => {
         // Display loader first
         let html = universitiesTemplate();
-        el.html(html);
+        el.innerHTML = html;
         try {
             const responseUniversities = await api.get('/universities');
             const universities = responseUniversities.data;
             console.log(universities);
             html = universitiesTemplate({universities});
-            el.html(html);
+            el.innerHTML = html;
         } catch (error) {
             showError(error);
         } finally {
             // Remove loader status
-            $('.loading').removeClass('loading');
+            removeLoadingClass();
+            
         }
     });
 
@@ -169,29 +188,39 @@ window.addEventListener('load', () => {
     this.router.navigateTo(window.location.pathname);
 
     // Highlight Active Menu on Refresh/Page Reload
-    const link = $(`a[href$='${window.location.pathname}']`);
-    link.addClass('active');
-
-    $('a').on('click', (event) => {
-        // Block browser page load
-        event.preventDefault();
-
-        // Highlight Active Menu on Click
-        const target = $(event.target);
-        $('.item').removeClass('active');
-        target.addClass('active');
-
-        // Navigate to clicked url
-        const href = target.attr('href');
-        const path = href.substr(href.lastIndexOf('/'));
-        this.router.navigateTo(path);
-    });
+    // const link = $(`a[href$='${window.location.pathname}']`);
+    // link.addClass('active');
+    //
+    //
+    // $('a').on('click', (event) => {
+    //     // Block browser page load
+    //     event.preventDefault();
+    //
+    //     // Highlight Active Menu on Click
+    //     const target = $(event.target);
+    //     $('.item').removeClass('active');
+    //     target.addClass('active');
+    //
+    //     // Navigate to clicked url
+    //     const href = target.attr('href');
+    //     const path = href.substr(href.lastIndexOf('/'));
+    //     this.router.navigateTo(path);
+    // });
 
 
 });
 
-function navigate(any){
-    this.router.navigateTo('/university/'+any+'/about');
+function navigate(any) {
+    this.router.navigateTo('/university/' + any);
 }
+
+function removeLoadingClass(){
+    let els = document.getElementsByClassName("loading");
+
+    Array.prototype.forEach.call(els, function(el) {
+        el.classList.remove('loading');
+    });
+}
+
 
 
