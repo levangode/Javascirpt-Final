2d84df47ab53ca3f092c9c7d12ca229a6179e59a
levangode <lgode14@freeuni.edu.ge>
2018-07-07 01:13:30 +0400

    small fix

--- src/main/java/ge/freeuni/project/models/UniversityReview.java
+++ src/main/java/ge/freeuni/project/models/UniversityReview.java
@@ -1,15 +1,13 @@
 package ge.freeuni.project.models;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 public class UniversityReview {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Lob
     private String review;
 
 
